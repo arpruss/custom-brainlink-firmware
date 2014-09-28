ead7464478dd8bb9375ba96447bd23ecb63aa1f2
arpruss <arpruss@gmail.com>
2014-09-28 15:20:18 -0500

    better led pwm scaling

--- led.c
+++ led.c
@@ -13,10 +13,6 @@ void init_led() {
 	// Set LED pins to output
 	LEDPORT.DIRSET |= REDLED | GREENLED | BLUELED;
 
-        TCD0.CCABUFL = 0xFF;
-        TCD0.CCBBUFL = 0xFF;
-        TCD0.CCCBUFL = 0xFF;
-
 	// Set the LED to 0, just in case
 	set_led(0,0,0);
 }
@@ -24,8 +20,16 @@ void init_led() {
 // Update the capture compare registers with new led values
 // Invert values since our LED is common anode
 void set_led(uint8_t red, uint8_t green, uint8_t blue) {
-	// Use just the high byte of the CC buffer, since we only desire 8 bit color
-	TCD0.CCABUFH = (uint8_t)255-red;
-	TCD0.CCBBUFH = (uint8_t)255-blue;
-	TCD0.CCCBUFH = (uint8_t)255-green;
+     // since the PWM duty cycle ranges from 0 to FFFF, but our
+     // RGB values from 0 to FF, we put the same RGB value for each
+     // of the two bytes of the duty cycle.
+        red = (uint8_t)255 - red;
+        green = (uint8_t)255 - green;
+        blue = (uint8_t)255 - blue;
+	TCD0.CCABUFH = red;
+	TCD0.CCABUFL = red;
+	TCD0.CCBBUFH = blue;
+	TCD0.CCBBUFL = blue;
+	TCD0.CCCBUFH = green;
+	TCD0.CCCBUFL = green;
 }
