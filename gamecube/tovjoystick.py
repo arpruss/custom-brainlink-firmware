06f6e911fdcf42653afa8f933b646d7f3226c2bf
arpruss <arpruss@gmail.com>
2017-06-18 14:41:38 -0500

    misc

--- gamecube/tovjoystick.py
+++ gamecube/tovjoystick.py
@@ -6,67 +6,82 @@ import time
 DEBUG = True
 GC_BUTTONS = ("a", "b", "x", "y", "z", "start", "l", "r")
         
-class PadOptions(object):
-    def __init__(self, buttonMap, combineShoulders = False):
+class GameCubeToVJoystick(object):
+    def __init__(self, buttonMap, combineShoulders = False, threshold=1):
         self.buttonMap = buttonMap
         self.combineShoulders = combineShoulders
+        self.threshold = threshold
+      
+    def buttonActive(self, state, button):
+        value = getattr(state, button)
+        if value == True or value == False:
+            return value
+        return value >= self.threshold
+      
+    @staticmethod
+    def remapAxis(value):
+        v1 = math.floor(value*32767./255+1.5)
+        return min(max(int(v1),1),32768)
+
+    def toVJoystick(self, joy, state):
+        joy.data.lButtons = sum(1<<i for i in range(len(self.buttonMap)) if self.buttonActive(state, self.buttonMap[i]))
+        joy.data.wAxisX = GameCubeToVJoystick.remapAxis(state.joyX)
+        joy.data.wAxisY = GameCubeToVJoystick.remapAxis(255-state.joyY)
+        joy.data.wAxisZRot = GameCubeToVJoystick.remapAxis(state.cX)
+        joy.data.wAxisYRot = GameCubeToVJoystick.remapAxis(255-state.cY)
+        if self.combineShoulders:
+            joy.data.wSlider = GameCubeToVJoystick.remapAxis((255+state.shoulderLeft - state.shoulderRight)/2)
+        else:
+            joy.data.wSlider = GameCubeToVJoystick.remapAxis(255-state.shoulderRight)
+        joy.data.wDial = GameCubeToVJoystick.remapAxis(255-state.shoulderLeft)
+        joy.update()
         
-PAD_OPTIONS_DEFAULT = PadOptions(GC_BUTTONS)
+        rid = joy.rID
+        
+        vector = [0,0]
+        if state.dUp:
+            vector[1] += 1
+        elif state.dRight:
+            vector[0] += 1
+        elif state.dDown:
+            vector[1] -= 1
+        elif state.dLeft:
+            vector[0] -= 1
+            
+        if vector[0] == 0 and vector[1] == 0:
+            pov1 = -1
+        else:
+            pov1 = (-9000+(100*int(math.floor(0.5+180/math.pi * math.atan2(vector[1],-vector[0]))))) % 36000
+            
+        joy._sdk.SetContPov(pov1,rid,1)
+        
+        pov2 = -1
+        px = state.cX - 128
+        py = state.cY - 128
+        if px*px + py*py >= 10*10:
+            pov2 = (-9000+int(math.floor(0.5+100*180/math.pi * math.atan2(py,-px)))) % 36000
+            
+        joy._sdk.SetContPov(pov2,rid,2)
+        
+PAD_OPTIONS_DEFAULT = GameCubeToVJoystick(GC_BUTTONS)
         
 maps = {
     "normal": PAD_OPTIONS_DEFAULT,
-    "normal-combined": PadOptions(GC_BUTTONS, combineShoulders = True),
-    "jetset": PadOptions(("a", "b", "y", "z", "x", "rightShoulder", "leftShoulder", "start"))
+    "normal-combined": GameCubeToVJoystick(GC_BUTTONS, combineShoulders = True),
+    "jetset": GameCubeToVJoystick(("a", "b", "y", "z", "x", "shoulderRight", "shoulderLeft", "start"))
     }
-
-def toVJoystick(joy, state, map=PAD_OPTIONS_DEFAULT):
-    joy.data.lButtons = sum(1<<i for i in range(len(map.buttonMap)) if getattr(state, map.buttonMap[i]))
-    joy.data.wAxisX = state.joyX-128
-    joy.data.wAxisY = 128-state.joyY
-    joy.data.wAxisZRot = state.cX
-    joy.data.wAxisYRot = state.cY
-    if map.combineShoulders:
-        joy.data.wSlider = state.shoulderLeft - state.shoulderRight
-        joy.data.wDial = 255-state.shoulderLeft
-    joy.update()
-    
-    rid = joy.rID
-    
-    vector = [0,0]
-    if state.dUp:
-        vector[1] += 1
-    elif state.dRight:
-        vector[0] += 1
-    elif state.dDown:
-        vector[1] -= 1
-    elif state.dLeft:
-        vector[0] -= 1
-        
-    if vector[0] == 0 and vector[1] == 0:
-        pov1 = -1
-    else:
-        pov1 = (-9000+(100*int(math.floor(0.5+180/math.pi * math.atan2(vector[1],-vector[0]))))) % 36000
-        
-    joy._sdk.SetContPov(pov1,rid,1)
-    
-    pov2 = -1
-    px = state.cX - 128
-    py = state.cY - 128
-    if px*px + py*py >= 10*10:
-        pov2 = (-9000+int(math.floor(0.5+100*180/math.pi * math.atan2(py,-px)))) % 36000
-        
-    joy._sdk.SetContPov(pov2,rid,2)
     
 if __name__ == '__main__':
     import sys
     import serial
+    import serial.tools.list_ports
     import os
     import atexit
     
     def err(message):
         if DEBUG: print(message)
 
-    port = "com3"    
+    port = None
     map = maps["normal"]
     delay = 5
     startVJoy = True
@@ -87,7 +102,10 @@ if __name__ == '__main__':
                 port = item
                 print (port)
         
-    print("Connecting to serial")
+    if port is None:
+        port = serial.tools.list_ports.comports()[0].device
+        
+    print("Connecting to serial on "+port)
     ser = serial.Serial(port, baudrate=115200, timeout=0.2)
 
     def disable():
@@ -101,12 +119,12 @@ if __name__ == '__main__':
     
     if startVJoy:
         print("Enabling vjoystick")
-        os.system(r'"c:\Program Files\vJoy\x64\vJoyConfig" 2 -f -a X Y RY RZ Sl0 Sl1 -b 16 -p 2')
+        os.system(r'"c:\Program Files\vJoy\x64\vJoyConfig" 2 -f -a X Y RZ RY Sl0 Sl1 -b 16 -p 2')
+#        os.system(r'"c:\Program Files\vJoy\x64\vJoyConfig" 2 -f -a Sl0 Sl1 -b 16')
         os.system(r'"c:\Program Files\vJoy\x64\vJoyConfig" enable on')
         atexit.register(disable)
     
     print("Running")
 
     joy = pyvjoy.VJoyDevice(2)
-    padOptions = PadOptions(map)
-    processGameCubeController(ser, delay, lambda s0,s1: toVJoystick(joy, s1, padOptions))
+    processGameCubeController(ser, delay, lambda s0,s1: map.toVJoystick(joy, s1))
